f7d59e29f0b7da6df4f7e2423d030d11767176c3
sherin34 <sherinjojupavaratty@gmail.com>
2023-10-02 22:53:11 +0530

    Initial commit

--- helpers/userHelper.js
+++ helpers/userHelper.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const { use } = require("../routes/login");
 const sqlite3 = require("sqlite3").verbose();
+let totalpoint = 0;
 const db = new sqlite3.Database(
   "./database/test.db",
   sqlite3.OPEN_READWRITE,
@@ -8,7 +9,7 @@ const db = new sqlite3.Database(
     if (err) return console.error(err.message);
   }
 );
-
+const { format } = require("date-fns");
 module.exports = {
   doSignup: (userData) => {
     return new Promise(async (resolve, reject) => {
@@ -25,28 +26,26 @@ module.exports = {
       resolve(userData);
     });
   },
-  
+
   checkRole: (userData) => {
     return new Promise(async (resolve, reject) => {
-      
       sql = "select * from user_index where user_id=?";
       db.get(sql, [userData.user_id], (err, rows) => {
         if (err) {
           reject(err);
         } else if (rows) {
           // resolve(rows);
-console.log(rows.role)
-          resolve(rows.role)
+          console.log(rows.role);
+          resolve(rows.role);
         } else {
           console.log("login failed due to user not exist");
-
-                  }
+        }
       });
     });
   },
   doLogin(userData) {
     sql = "SELECT * FROM user WHERE user_id=?";
-    let response={}
+    let response = {};
     return new Promise((resolve, reject) => {
       db.get(sql, [userData.user_id], (err, rows) => {
         if (err) {
@@ -74,4 +73,175 @@ console.log(rows.role)
       });
     });
   },
+  submitDailyReport: (userData, username) => {
+    return new Promise(async (resolve, reject) => {
+      let submitDate = new Date();
+      let formatedDate = format(submitDate, "yyyy-MM-dd");
+
+      console.log(formatedDate);
+      //find total points if any
+      totalPoints(userData).then((response) => {
+        totalpoint = response;
+      });
+
+      //check if any entry is there in the same day with same Userid
+      let entryCheckSQL = "SELECT * FROM reporting WHERE user_id=? AND date=?";
+      db.get(entryCheckSQL, [username, formatedDate], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else if (rows) {
+          // means, there is already an entry for the same day for same user id
+          let updateReportSQL =
+            "UPDATE reporting SET user_id=?,date=?, life_rep=?, health_rep=?, general_rep=?, rd_rep=?, term_deposit_rep=?, casa_rep=?, sb_silver_rep=?, cd_rep=?, housing_loan_rep=?, vehicle_loan_rep=?, business_loan_rep=?, total_points=? WHERE user_id=? AND date=?";
+          db.run(
+            updateReportSQL,
+            [
+              username,
+              formatedDate,
+              userData.lifeInsurance,
+              userData.healthInsurance,
+              userData.generalInsurance,
+              userData.rd,
+              userData.termDeposit,
+              userData.casa,
+              userData.sbSilver,
+              userData.cdSilver,
+              userData.housingLoanLead,
+              userData.vehicleLoanLead,
+              userData.businessLoanLead,
+              totalpoint,
+              username,
+              formatedDate,
+            ],
+            (err) => {
+              if (err) {
+                return console.error(err.message);
+              } else {
+                console.log("already existed entry updated");
+                resolve();
+              }
+            }
+          );
+        } else {
+          //calculate total points
+          // totalpoint=totalPoints(userData);
+
+          //write new entry in table
+
+          sql =
+            "INSERT INTO reporting (user_id, date, life_rep, health_rep, general_rep, rd_rep, term_deposit_rep, casa_rep, sb_silver_rep, cd_rep, housing_loan_rep, vehicle_loan_rep, business_loan_rep, total_points) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+          db.run(
+            sql,
+            [
+              username,
+              formatedDate,
+              userData.lifeInsurance,
+              userData.healthInsurance,
+              userData.generalInsurance,
+              userData.rd,
+              userData.termDeposit,
+              userData.casa,
+              userData.sbSilver,
+              userData.cdSilver,
+              userData.housingLoanLead,
+              userData.vehicleLoanLead,
+              userData.businessLoanLead,
+              totalpoint,
+            ],
+            (err) => {
+              if (err) return console.error(err.message);
+              else {
+                console.log("new entry in reporting added");
+                resolve();
+              }
+            }
+          );
+        }
+      });
+    });
+  },
+  totalPoints: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      pointsSql = "select * from points";
+      db.get(pointsSql, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else if (rows) {
+          // resolve(rows);
+          console.log(rows);
+        }
+      });
+    });
+  },
+
+  showDailyReportBM: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      // sql = "select * from reporting";
+      let today = new Date();
+      today = format(today, "yyyy-MM-dd");
+      sql =
+        "SELECT u.name, r.user_id, r.date, r.life_rep, r.health_rep, r.general_rep, r.rd_rep, r.term_deposit_rep, r.casa_rep, r.sb_silver_rep, r.cd_rep, r.housing_loan_rep, r.vehicle_loan_rep, r.business_loan_rep, r.total_points FROM user AS u JOIN reporting AS r ON u.user_id = r.user_id WHERE r.date = ? ORDER BY r.date";
+      db.all(sql, [today], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else if (rows) {
+          // resolve(rows);
+
+          resolve(rows);
+        } else {
+          console.log("nothing in the table");
+        }
+      });
+    });
+  },
+  filterBM: (userData) => {
+    return new Promise(async (resolve, reject) => {
+      // sql = "SELECT u.name, r.user_id, r.date, r.life_rep, r.health_rep, r.general_rep, r.rd_rep, r.term_deposit_rep, r.casa_rep, r.sb_silver_rep, r.cd_rep, r.housing_loan_rep, r.vehicle_loan_rep, r.business_loan_rep, r.total_points FROM user AS u JOIN reporting AS r ON u.user_id = r.user_id WHERE r.date BETWEEN ? AND ?";
+      sql =
+        "SELECT u.name AS name, SUM(r.life_rep) AS life_rep, SUM(r.health_rep) AS health_rep, SUM(r.general_rep) AS general_rep, SUM(r.rd_rep) AS rd_rep, SUM(r.term_deposit_rep) AS term_deposit_rep, SUM(r.casa_rep) AS casa_rep, SUM(r.sb_silver_rep) AS sb_silver_rep, SUM(r.cd_rep) AS cd_rep, SUM(r.housing_loan_rep) AS housing_loan_rep, SUM(r.vehicle_loan_rep) AS vehicle_loan_rep, SUM(r.business_loan_rep) AS business_loan_rep, SUM(r.total_points) AS total_points FROM user AS u JOIN reporting AS r ON u.user_id = r.user_id WHERE r.date BETWEEN ? AND ? GROUP BY u.name ORDER BY u.name";
+
+      db.all(sql, [userData.fromDate, userData.toDate], (err, rows) => {
+        if (err) {
+          reject(err);
+        } else if (rows) {
+          // resolve(rows);
+          // console.log(rows);
+
+          resolve(rows);
+        } else {
+          console.log("nothing in the table");
+        }
+      });
+    });
+  },
+
 };
+
+function totalPoints(userData) {
+  return new Promise(async (resolve, reject) => {
+    pointsSql = "select * from points";
+    db.get(pointsSql, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else if (rows) {
+        totalpoint =
+          userData.lifeInsurance * rows.life +
+          userData.healthInsurance * rows.health +
+          userData.generalInsurance * rows.general +
+          userData.rd * rows.rd +
+          userData.termDeposit * rows.term_deposit +
+          userData.casa * rows.casa +
+          userData.sbSilver * rows.sb_silver +
+          userData.cdSilver * rows.cd +
+          userData.housingLoanLead * rows.home_loan +
+          userData.vehicleLoanLead * rows.vehicle_loan +
+          userData.businessLoanLead * rows.business_loan;
+
+        // resolve(rows);
+        // console.log(totalpoint)
+        return totalpoint;
+      }
+    });
+  });
+}
--- routes/login.js
+++ routes/login.js
@@ -7,6 +7,8 @@ Handlebars = require("handlebars");
 let sql;
 let password;
 let role;
+let username;
+let bm_data; //DATA fetched from reported users
 
 const db = new sqlite3.Database(
   "./database/test.db",
@@ -24,6 +26,7 @@ router.get("/", function (req, res, next) {
   res.render("login", { admin: false, user: false });
 });
 router.get("/login", function (req, res, next) {
+  username = "";
   res.render("login", { admin: false, user: false });
 });
 router.get("/signup", function (req, res, next) {
@@ -44,13 +47,19 @@ router.post("/login", (req, res) => {
       if (rows == "user") {
         role.user = true;
       } else if (rows == "bm") {
-        role.bm = true;
+        role.bm = true; //to set headers
+
+        userHelpers.showDailyReportBM(req.body, username).then((response) => {
+          // console.log(response)
+
+          bm_data = response;
+        });
       } else if (rows == "ch") {
         role.ch = true;
-      } else if (rows = "rh") {
+      } else if ((rows = "rh")) {
         role.rh = true;
-      }else{
-        console.log("role didn't obtained correctly")
+      } else {
+        console.log("role didn't obtained correctly");
       }
     })
     .catch((error) => {
@@ -63,13 +72,89 @@ router.post("/login", (req, res) => {
       // Process the data after the SQL query is complete
       // console.log(rows);
       // Continue with other operations here
+      username = rows.user.user_id;
       let userData = rows;
       console.log(userData.user.user_id);
-
-      res.render("user", { userData, user: true, role });
+      //getting result rwo for bm login
+      
+      res.render("user", { userData, user: true, role, bm_data });
     })
     .catch((error) => {
       console.error("Error:", error.message);
     });
 });
+
+router.post("/submitDailyReport", (req, res) => {
+  // console.log(req.body);
+  userHelpers.submitDailyReport(req.body, username).then((response) => {
+    res.render("./partials/user_body", { username, user: true, role });
+    // console.log("Login Success")
+    // console.log(response)
+  });
+});
+
+//filter userdata showing to BM
+router.post("/applyfiltersBM", (req, res) => {
+  // console.log(req.body);
+  const scrollToDailyReporting = "dailyReporting";
+  //to know which button pressed in filter in show daily report BM
+  const filterButtonAction = req.body.buttonAction;
+
+  if (filterButtonAction === "applyFilter") {
+    //ccheck if login has done?
+    if (username === "") {
+      res.send("Login first");
+    } else {
+      userHelpers.filterBM(req.body, username, bm_data).then((response) => {
+        bm_data = response;
+        //processing chartgs
+
+        const sqlResult = bm_data;
+
+        const chartData = {
+          labels: sqlResult.map((item) => item.name), // Array of names
+          datasets: [
+            {
+              label: "Total Points",
+              data: sqlResult.map((item) => item.total_points), // Array of total_points
+              borderColor: "rgba(75, 192, 192, 1)",
+              borderWidth: 1,
+              fill: false,
+            },
+          ],
+        };
+
+        console.log(chartData.datasets);
+        res.render("./partials/bm_body", {
+          username,
+          user: true,
+          role,
+          bm_data,
+          scrollToDailyReporting,
+          chartData,
+        });
+      });
+    }
+  } else if (filterButtonAction === "resetFilter") {
+    userHelpers.showDailyReportBM(req.body, username).then((response) => {
+      // console.log(response)
+
+      bm_data = response;
+      //ccheck if login has done?
+
+      if (username === "") {
+        res.send("Login first");
+      } else {
+        res.render("./partials/bm_body", {
+          username,
+          user: true,
+          role,
+          bm_data,
+          scrollToDailyReporting,
+        });
+      }
+    });
+  }
+});
+
 module.exports = router;
